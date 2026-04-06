3754a14f9a073fbba2226c64b65d781ccde644dd
KAbhishek2526 <kokkiligaddaabhishek2006@gmail.com>
2026-04-06 17:55:52 +0530

    fix mongodb connection options

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,7 @@ app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5001;
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB successfully connected');
   })
